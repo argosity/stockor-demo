d56ce403ebfb989b0ff8c821d347516b573376ec
Nathan Stitt <nathan@stitt.org>
2016-03-27 21:08:37 -0500

    mend

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 set :application, 'stockor'
 set :repo_url, 'git@github.com:argosity/stockor-demo-access.git'
 
-set :deploy_to, '/srv/www/stockor-next-gen'
+set :deploy_to, '/srv/www/stockor-demo'
 
 namespace :deploy do
     desc 'Restart application'
